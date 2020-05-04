f26f7b470e89827453e7ebefbd15f8fd28076572
KucingMaboek <45931684+KucingMaboek@users.noreply.github.com>
2020-05-04 21:27:37 +0700

    common updet

--- src/main/java/controllers/LapInstalasiController.java
+++ src/main/java/controllers/LapInstalasiController.java
@@ -10,8 +10,12 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import models.InstalasiModel;
+import utils.Helper;
 
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ResourceBundle;
 
 public class LapInstalasiController implements Initializable {
@@ -22,10 +26,13 @@ public class LapInstalasiController implements Initializable {
     public TableColumn<InstalasiModel, String> chatID;
 
     @FXML
-    public TableColumn<InstalasiModel, String> title;
+    public TableColumn<InstalasiModel, String> nama;
 
     @FXML
     public TableColumn<InstalasiModel, String> date;
+
+    @FXML
+    public TableColumn<InstalasiModel, String> status;
 
     MainController mainCopyController;
 
@@ -33,18 +40,43 @@ public class LapInstalasiController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
 
         //make sure the property value factory should be exactly same as the e.g getStudentId from your model class
-        chatID.setCellValueFactory(new PropertyValueFactory<>("chatID"));
-        title.setCellValueFactory(new PropertyValueFactory<>("title"));
+        chatID.setCellValueFactory(new PropertyValueFactory<>("chatId"));
+        nama.setCellValueFactory(new PropertyValueFactory<>("nama"));
         date.setCellValueFactory(new PropertyValueFactory<>("date"));
+        status.setCellValueFactory(new PropertyValueFactory<>("status"));
         //add your data to the table here.
-        for (int i = 0; i < 100; i++) {
-            InstalasiModels.add(new InstalasiModel(
-                    "@dummy_" + Integer.toString(i),
-                    "pasang wifi dong",
-                    "dummy"
-            ));
+        try {
+            String query = "select * from `instalasi_listrik` where status is not null";
+            Statement stats = Helper.connectDatabase().createStatement();
+            ResultSet rs = stats.executeQuery(query);
+            while (rs.next()) {
+                InstalasiModels.add(new InstalasiModel(
+                        rs.getInt("id"),
+                        rs.getString("date"),
+                        rs.getString("chatId"),
+                        rs.getString("nama"),
+                        rs.getString("provinsi"),
+                        rs.getString("kota"),
+                        rs.getString("kecamatan"),
+                        rs.getString("kelurahan"),
+                        rs.getString("alamat"),
+                        rs.getString("nomorTelepon"),
+                        rs.getString("email"),
+                        rs.getString("nik"),
+                        rs.getString("npwp"),
+                        rs.getString("layanan"),
+                        rs.getString("peruntukan"),
+                        rs.getString("daya"),
+                        rs.getString("tokenPerdana"),
+                        rs.getString("status")
+                ));
+            }
+            tbInstalasi.setItems(InstalasiModels);
+            rs.close();
+            stats.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        tbInstalasi.setItems(InstalasiModels);
     }
 
     // add your data here from any source
@@ -57,9 +89,10 @@ public class LapInstalasiController implements Initializable {
     public void cell_onClick(MouseEvent mouseEvent) {
         if (mouseEvent.getClickCount() == 2) //Checking double click
         {
-            System.out.println(tbInstalasi.getSelectionModel().getSelectedItem().getChatID());
-            System.out.println(tbInstalasi.getSelectionModel().getSelectedItem().getTitle());
+            System.out.println(tbInstalasi.getSelectionModel().getSelectedItem().getChatId());
+            System.out.println(tbInstalasi.getSelectionModel().getSelectedItem().getNama());
             System.out.println(tbInstalasi.getSelectionModel().getSelectedItem().getDate());
+            System.out.println(tbInstalasi.getSelectionModel().getSelectedItem().getStatus());
         }
     }
 }
--- src/main/java/models/InstalasiModel.java
+++ src/main/java/models/InstalasiModel.java
@@ -2,39 +2,52 @@ package models;
 
 public class InstalasiModel {
 
-    private String chatID;
-    private String title;
+    private int id;
     private String date;
-//    private String layanan;
-//    private String peruntukan;
-//    private int daya;
-//    private int tokenPerdana;
-//    private String alamat;
-//    private String nama;
-//    private String NoKtp;
-//    private String NoHp;
+    private String chatId;
+    private String nama;
+    private String provinsi;
+    private String kota;
+    private String kecamatan;
+    private String kelurahan;
+    private String alamat;
+    private String nomorTelepon;
+    private String email;
+    private String nik;
+    private String npwp;
+    private String layanan;
+    private String peruntukan;
+    private String daya;
+    private String tokenPerdana;
+    private String status;
 
-
-    public InstalasiModel(String chatID, String title, String date) {
-        this.chatID = chatID;
-        this.title = title;
+    public InstalasiModel(int id, String date, String chatId, String nama, String provinsi, String kota, String kecamatan, String kelurahan, String alamat, String nomorTelepon, String email, String nik, String npwp, String layanan, String peruntukan, String daya, String tokenPerdana, String status) {
+        this.id = id;
         this.date = date;
+        this.chatId = chatId;
+        this.nama = nama;
+        this.provinsi = provinsi;
+        this.kota = kota;
+        this.kecamatan = kecamatan;
+        this.kelurahan = kelurahan;
+        this.alamat = alamat;
+        this.nomorTelepon = nomorTelepon;
+        this.email = email;
+        this.nik = nik;
+        this.npwp = npwp;
+        this.layanan = layanan;
+        this.peruntukan = peruntukan;
+        this.daya = daya;
+        this.tokenPerdana = tokenPerdana;
+        this.status = status;
     }
 
-    public String getChatID() {
-        return chatID;
+    public int getId() {
+        return id;
     }
 
-    public void setChatID(String chatID) {
-        this.chatID = chatID;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getDate() {
@@ -43,5 +56,133 @@ public class InstalasiModel {
 
     public void setDate(String date) {
         this.date = date;
+    }
+
+    public String getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(String chatId) {
+        this.chatId = chatId;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getProvinsi() {
+        return provinsi;
+    }
+
+    public void setProvinsi(String provinsi) {
+        this.provinsi = provinsi;
+    }
+
+    public String getKota() {
+        return kota;
+    }
+
+    public void setKota(String kota) {
+        this.kota = kota;
+    }
+
+    public String getKecamatan() {
+        return kecamatan;
+    }
+
+    public void setKecamatan(String kecamatan) {
+        this.kecamatan = kecamatan;
+    }
+
+    public String getKelurahan() {
+        return kelurahan;
+    }
+
+    public void setKelurahan(String kelurahan) {
+        this.kelurahan = kelurahan;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getNomorTelepon() {
+        return nomorTelepon;
+    }
+
+    public void setNomorTelepon(String nomorTelepon) {
+        this.nomorTelepon = nomorTelepon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNik() {
+        return nik;
+    }
+
+    public void setNik(String nik) {
+        this.nik = nik;
+    }
+
+    public String getNpwp() {
+        return npwp;
+    }
+
+    public void setNpwp(String npwp) {
+        this.npwp = npwp;
+    }
+
+    public String getLayanan() {
+        return layanan;
+    }
+
+    public void setLayanan(String layanan) {
+        this.layanan = layanan;
+    }
+
+    public String getPeruntukan() {
+        return peruntukan;
+    }
+
+    public void setPeruntukan(String peruntukan) {
+        this.peruntukan = peruntukan;
+    }
+
+    public String getDaya() {
+        return daya;
+    }
+
+    public void setDaya(String daya) {
+        this.daya = daya;
+    }
+
+    public String getTokenPerdana() {
+        return tokenPerdana;
+    }
+
+    public void setTokenPerdana(String tokenPerdana) {
+        this.tokenPerdana = tokenPerdana;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
     }
 }
