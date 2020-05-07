d7340b51d10f8cb80591b0758508e4dba4fdacc6
KucingMaboek <45931684+KucingMaboek@users.noreply.github.com>
2020-05-07 22:55:53 +0700

    common updet

--- src/main/java/controllers/BerandaController.java
+++ src/main/java/controllers/BerandaController.java
@@ -2,6 +2,7 @@ package controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.text.Text;
 import utils.Helper;
 
@@ -13,28 +14,23 @@ public class BerandaController {
     @FXML
     private Text txt_internetConnection, txt_botConnection, txt_twitterConnection, txt_databaseConnection;
 
-
-
     @FXML
     void btn_botReconnect(ActionEvent event) {
-
     }
-
     @FXML
     void btn_databaseReconnect(ActionEvent event) {
         Helper.connectDatabase();
         txt_databaseConnection.setText(Helper.getDbConnectionStats());
     }
-
     @FXML
     void btn_internetReconnect(ActionEvent event) {
-
+    }
+    @FXML
+    void btn_twitterReconnect(ActionEvent event) {
     }
 
     @FXML
-    void btn_twitterReconnect(ActionEvent event) {
-
-    }
+    private AnchorPane ap_status_net;
 
 
     @FXML
